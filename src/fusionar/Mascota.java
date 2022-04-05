c3b476e0b50610f7c7e3f1232c7d50b98cc5dce8
‘Arturo’ <arturogomez-barreda@iessampedro.org>
2022-04-05 18:41:35 +0100

    he cambiado el la rama-mascota la clase mascota el mensaje

--- src/fusionar/Mascota.java
+++ src/fusionar/Mascota.java
@@ -28,7 +28,7 @@ public class Mascota {
 	}
 
 	public void MandarMensaje() {
-		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no" + " del medio " + this.medio);
 	}
 
 }
